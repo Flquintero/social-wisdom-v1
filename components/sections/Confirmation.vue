5bd6f497b431029346c5a53a08d6767198a84c01
Francisco Quintero <flquintero1@gmail.com>
2023-09-28 15:27:45 -0400

    Added form in confirmation

--- components/sections/Confirmation.vue
+++ components/sections/Confirmation.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="confirmation-content">
     <div class="confirmation-content__header">
-      <h2>We will work on your request! ❤️</h2>
+      <!-- <h1>We will work on your request! ❤️</h1> -->
       <h3>{{ currentExpert }} is not on the platform yet!</h3>
-      <h3>But with your help we can get them on, and that way help others!</h3>
+      <h3>
+        But with your help we can get them on, and that way help others! ❤️
+      </h3>
     </div>
     <div class="confirmation-content__bid">
-      <h2>
+      <h1 class="confirmation-content__bid__title">
         We believe that if you offer to pay for the answer, we can get them to
         send you a video with it.
-      </h2>
+      </h1>
+      <div class="confirmation-content__bid__form">
+        <h3>If so, how much would you be willing to pay?</h3>
+        <div class="confirmation-content__bid__form__input">
+          💲&nbsp;&nbsp;<BaseInput
+            @input="setAmount($event)"
+            v-bind="{ placeholder: 'Enter Amount...', type: 'number' }"
+          />
+        </div>
+      </div>
     </div>
-    <div class="confirmation-content__form"></div>
-    <div class="confirmation-content__actions"></div>
+    <div class="confirmation-content__details">
+      <h3>
+        Also, we would need your info, to send you the answer (nothing else):
+      </h3>
+      <div class="confirmation-content__details__form">
+        <BaseInput v-bind="{ placeholder: 'Name', type: 'text' }" />
+        <BaseInput v-bind="{ placeholder: 'Email', type: 'text' }" />
+      </div>
+    </div>
+    <div class="confirmation-content__actions">
+      <BaseButton button-text="Get Me The Answer! 💪" variant="primary" />
+    </div>
   </div>
 </template>
 <script lang="ts">
@@ -56,12 +77,49 @@ export default defineComponent({
 </script>
 <style lang="scss" scoped>
 .confirmation-content {
+  padding: 20px;
   &__header {
     text-align: center;
   }
   &__bid {
     text-align: center;
     margin: 30px 0;
+    &__title {
+      width: 100%;
+      max-width: 800px;
+      margin: auto;
+    }
+    &__form {
+      margin-top: 30px;
+
+      &__input {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 20px;
+      }
+    }
+  }
+  &__details {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin-top: 20px;
+    &__form {
+      margin-top: 20px;
+      width: 100%;
+      max-width: 400px;
+      div {
+        margin-bottom: 15px;
+      }
+    }
+  }
+  &__actions {
+    width: 100%;
+    max-width: 250px;
+    height: 35px;
+    margin: 10px auto;
   }
 }
 </style>
