7237a7bf1a2bfcaa846c620a1bf884e3ecaa82bf
Francisco Quintero <flquintero1@gmail.com>
2023-10-04 10:43:43 -0400

    fixed typo

--- components/elements/Button.vue
+++ components/elements/Button.vue
@@ -9,7 +9,7 @@
     :disabled="disabled"
   >
     <div class="c-btn__loading-icon" v-if="loading">
-      <img src="~/public/img/ButtonLoaderIcon.svg?inline" />
+      <img src="~/public/img/buttonLoaderIcon.svg?inline" />
     </div>
     <slot v-else>
       <!-- <c-icon v-if="iconConfig" :style="iconPosition" v-bind="iconConfig" /> -->
